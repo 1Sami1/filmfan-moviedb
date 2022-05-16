5d6dafe5d1e6faa0daf9cb2488cce5e4c13fc6c5
Sami Adli <samiadli00@gmail.com>
2022-05-16 10:18:43 -0700

    most updated version

--- src/components/BrowseBy.js
+++ src/components/BrowseBy.js
@@ -3,14 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 function BrowseBy() {
   return (
-    <nav className='browse-by'>
-        <ul>
-            <li><NavLink to="/sort/top-rated">Top Rated</NavLink></li>
-            <li><NavLink to="/sort/popular">Popular</NavLink></li>
-            <li><NavLink to="/sort/upcoming">Upcoming</NavLink></li>
-            <li><NavLink to="/sort/now-playing">Now Playing</NavLink></li>
+        <ul className='browse-by'>
+            <li className='facet-link'><NavLink to="/sort/top-rated">Top Rated</NavLink></li>
+            <li className='facet-link'><NavLink to="/sort/popular">Popular</NavLink></li>
+            <li className='facet-link'><NavLink to="/sort/upcoming">Upcoming</NavLink></li>
+            <li className='facet-link'><NavLink to="/sort/now-playing">Now Playing</NavLink></li>
         </ul>
-    </nav>
+    
   )
 }
 
