ceb9b158755094a520b07b0eac6791f8eb8a2d68
Sami Adli <96108431+1Sami1@users.noreply.github.com>
2022-08-16 21:54:51 -0700

    Added Load More button and to top

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,57 +1,64 @@
 import BrowseBy from "../components/BrowseBy";
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import Movies from '../components/Movies';
 import { API_KEY } from '../globals/globals';
 
 
 function HomePage({sort}) {
-    const [moviesData, setMoviesData] = useState(false);
-    const [error, setError] = useState(false);
+        const selectedMovieFilter = sort;
+    // Movie List
+    const [moviesData, setMovieList] = useState(false);
+    // Flag for if more pages can be loaded
+    const [canLoadMore, setCanLoadMore] = useState(true);
+    // Last loaded page
+    const [moviePage, setMoviePage] = useState(1);
 
-    const errorMessage = 'Oh no...Something went wrong...Please try again later.';
+    const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // for smoothly scrolling
+    });
+    };
 
+
+
+    const fetchMovies = useCallback(async (page) => {
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${selectedMovieFilter}?api_key=${API_KEY}&language=en-US&page=${page}`);
+    const data = await res.json();
+    const selectedMovies = data.results.splice(0,12);
+    if (page === 1) {
+        setMovieList(selectedMovies)
+    } else {
+        setMovieList(moviesData => [...moviesData, ...selectedMovies]);
+    }
+    // If the total page limit has been reached set the canLoadMore flag
+    if (page >= data.total_pages) {
+        setCanLoadMore(false)
+    }
+    }, [selectedMovieFilter])
+
+    // Handle loadMore button
+    const handleLoadMore = () => {
+    fetchMovies(moviePage + 1);
+    setMoviePage(moviePage + 1);
+    }
+
+    // Re-fetch movies if the selectedMovieFilter changes - this will also occur on page load
     useEffect(() => {
+    fetchMovies(1)
+    setMoviePage(1);
+    setCanLoadMore(true)
+    }, [fetchMovies, selectedMovieFilter])
+    
 
-        const fetchMovies = async () => {
-
-            // The "catch()" will only run if the request is bad...
-            // for example: no response at all from the server...
-            const res = await fetch(`https://api.themoviedb.org/3/movie/${sort}?api_key=${API_KEY}&language=en-US&page=1`)
-                                .catch(err => {
-                                    moviesData !== false && setMoviesData(false);
-                                    setError(errorMessage);
-                                })
-            
-            const data = await res.json();
-
-            // This code will only run...if we get a response (good or bad) from the API
-            if(data.success === false){
-                // Bad response from the API...
-                // Reset the movies if it is not false...
-                moviesData !== false && setMoviesData(false);
-                error !== false && setError(false);
-                setError(errorMessage);
-            }else{
-                // Good response fromt he API
-                // Reset the error if it is not false...
-                error !== false && setError(false);
-                // Grab the first 12 movies from the API
-                const first12Movies = data.results.splice(0,24);
-                setMoviesData(first12Movies);
-            }
-            
-
-        }
-
-        fetchMovies()
-
-
-    }, [sort]);
     return(
         <section className="home-page">
                 <BrowseBy /> 
-                {error !== false && <p className="api-error">{error}</p>}
-            {moviesData !== false && <Movies moviesData={moviesData}  />}         
+            {moviesData  && <Movies moviesData={moviesData}  />}
+            <div className="btns-div">
+                {canLoadMore && <button onClick={handleLoadMore} className="load-more">Load More</button>}         
+            <button className="top-btn" onClick={scrollToTop}>TOP</button>
+            </div>
             
         </section>
     )
