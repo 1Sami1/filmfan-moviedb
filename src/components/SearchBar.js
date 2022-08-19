b93d6cea0d17447c6ec92515db41aff72018ce99
Sami Adli <96108431+1Sami1@users.noreply.github.com>
2022-08-18 22:10:14 -0700

    Added Search Bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import {useNavigate} from 'react-router-dom';
+import {useState} from 'react';
+
+
+const SearchBar = (movie) => {
+
+    const [searchInput, setSearchInput] = useState('');
+
+    const navigate = useNavigate();
+
+    const searchItems = (searchValue) => {
+        let uriEncodedSearchValue = encodeURIComponent(searchValue);
+        setSearchInput(uriEncodedSearchValue);
+    }
+
+    function submitSearch(e){
+        e.preventDefault();
+        if(searchInput === ''){
+            return;
+        }
+        e.target.elements.search.value = '';
+        navigate(`/search?q=${searchInput}`, { replace: true })
+    }
+
+    return(
+            <div className='search-wrapper'>
+                <form action="#" autoComplete='off' onSubmit={submitSearch}>
+                    <input className='search-bar' type="text" placeholder="Search for a movie ..." id="search" name="search" onChange={(e) => searchItems(e.target.value)} />
+                </form>
+            </div>
+    )
+}
+
+export default SearchBar;
