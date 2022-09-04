30d356e554e316fa1a4ff243bdf9fa38cd8776a8
Sami Adli <96108431+1Sami1@users.noreply.github.com>
2022-09-03 18:43:47 -0700

    Movie card hover restyling

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -24,21 +24,18 @@ function MovieCard( { movie, isFav } ) {
 
     const MOVIE_OVERVIEW = movie.overview;
 
-    const [isShown, setIsShown] = useState(false);
+    
 
     return(
         <>
         <div className='movie-card' >
 
-            <div className="movie-poster"  
-                 onMouseEnter={() => setIsShown(true)} 
-                 onMouseLeave={() => setIsShown(false)} >
+            <div className="movie-poster"  >
 
                 {movie.poster_path === null ? 
                     <img src={noPoster} alt="No poster available" /> :
                     <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
                 }
-                {isShown && (
                     <div className='on-hover'>
                         <span className='black-bg'></span>
                         <h3 className='hover-title'>{movie.title.length > 25 ? `${movie.title.substring(0, 25)}...` : movie.title}</h3>
@@ -47,7 +44,6 @@ function MovieCard( { movie, isFav } ) {
                         <Link className='hover-link' to={`/movie/${movie.id}`}>More Info</Link>
                         
                     </div>
-                )}
             </div>
             
         </div>
